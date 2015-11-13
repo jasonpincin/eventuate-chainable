cf722ef04643bc6cb8f2900fb5eb2aea06ad5178
Jason Pincin <jason@pincin.com>
2015-11-12 19:10:37 -0500

    added test [ci skip]

--- test/destroy.js
+++ test/destroy.js
@@ -18,3 +18,21 @@ test('should be destroyed with upstream', function (t) {
     event.destroy()
     t.ok(ucEvent.isDestroyed(), 'destroyed after upstream is destroyed')
 })
+
+test('does not lazily consume after being destroyed', function (t) {
+    t.plan(2)
+
+    var eventuateMap = chainable(function (eventuate, options, map) {
+        return function upstreamConsumer (data) {
+            eventuate.produce(map(data))
+        }
+    })
+
+    var event = eventuate()
+    var ucEvent = eventuateMap(event, function (data) { return data.toUpperCase() })
+
+    t.notOk(event.hasConsumer(ucEvent.upstreamConsumer), 'consumer NOT in upstream')
+    ucEvent.destroy()
+    ucEvent(function () {})
+    t.notOk(event.hasConsumer(ucEvent.upstreamConsumer), 'consumer NOT in upstream')
+})
